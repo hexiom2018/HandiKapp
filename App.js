d4e8b7d2531366ed775ed591f45ca391cdc50f08
Talha-Jawed <jawedtalha0@gmail.com>
2019-03-26 00:01:49 +0500

    init

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
 import { Provider } from 'react-redux';
 import store from './src/Store/store';
@@ -9,18 +8,8 @@ export default class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-      <Navigation/>
+        <Navigation />
       </Provider>
-
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
