bf203497c4a962ca6abd1e82d4fc290634d11b35
nabeelRrehman <nabeelshahid2@gmail.com>
2019-04-06 22:00:24 +0500

    add data to firebase

--- src/Store/actions/authAction.js
+++ src/Store/actions/authAction.js
@@ -119,10 +119,14 @@ export function UpdateUserProfile(items, userUid) {
 }
 
 
-export function AddParkingSpace(items, userUid) {
+export function AddParkingSpace(data, user) {
     return dispatch => {
         return new Promise(function (resolve, reject) {
-            db.ref('places')
+            db.ref('/places/' + user).set(data).then(() => {
+                resolve()
+            }).catch(() => {
+                reject()
+            })
         })
     }
 }
