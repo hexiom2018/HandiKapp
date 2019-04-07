908b66269dfd6d8a5c2328afea30dbb0287314cf
SyedUzairAlii <maazuzair3322@gmail.com>
2019-04-08 01:34:22 +0500

    console.log-yellowbox true

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navigation from './navigation';
 import { Provider } from 'react-redux';
 import store from './src/Store/store';
+console.disableYellowBox = true;
 
 
 export default class App extends React.Component {
